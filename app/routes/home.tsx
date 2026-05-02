6df96f576a5532483ba92cf6647d5563acdc061e
Welbeck <michael.welbeck@st.rmu.edu.gh>
2026-05-02 03:12:05 +0000

    firsts push + homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
+import {ArrowRight, Clock, Layers} from "lucide-react";
+import {Button} from "../../components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -22,6 +24,82 @@ export default function Home() {
             </div>
             <h1>Build beautiful spaces at the speed to thought with Vision </h1>
 
+            <p className="subtitle">
+                Vision is an AI-first design environment that helps visualize, render, and ship architectural projects faster than ever.
+            </p>
+            <div className="actions">
+                <a href="#upload" className="cta">
+                    start Building <ArrowRight
+                    className="icon" />
+                </a>
+
+                <Button variant="outline" size="lg"
+                className="demo">
+                    Watch Demo
+                </Button>
+            </div>
+
+            <div id="upload" className="upload-shell">
+                <div className="grid-overlay" />
+
+                <div className="upload-card">
+                    <div className="upload-head">
+                        <div className="upload-icon">
+                            <Layers className = "icon" />
+                        </div>
+
+                        <h3>Upload your floor plan</h3>
+                        <p>Supports JPG, PNG, and SVG formats up to 10MB</p>
+                    </div>
+
+                    <p>Upload images</p>
+                </div>
+            </div>
+        </section>
+
+        <section className="projects">
+            <div className="section-inner">
+                <div className="section-head" >
+                    <div className="copy" >
+                        <h2>Projects</h2>
+                        <p>Your latest work and shared community projects, all in one place.</p>
+                    </div>
+                </div>
+
+                <div className="projects-grid">
+                    <div className="project-card group">
+                        <div className="preview">
+                            <img
+                            src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                                   alt="Project"
+                            />
+
+                            <div className="badbe">
+                                <span>Community</span>
+                            </div>
+
+                            <div className="card-body">
+                                <div>
+                                    <h3>Project Manhattan</h3>
+                                    <p>A 3D model of Manhattan</p>
+
+                                    <div className="meta">
+                                        <Clock size={12} />
+                                        <span>{new Date('01.01.2024').toLocaleTimeString()}</span>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className="arrow">
+                                <ArrowRight size={18}/>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </section>
       </div>
 
